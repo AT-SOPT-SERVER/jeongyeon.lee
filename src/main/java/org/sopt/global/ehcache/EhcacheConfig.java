91b7e52fcaab3276cc3849ef79d518ab14dc5f3d
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-13 16:20:43 +0900

    [Feat] EhcacheConfig 추가

---
+++ src/main/java/org/sopt/global/ehcache/EhcacheConfig.java
@@ -0,0 +1,25 @@
+package org.sopt.global.ehcache;
+
+import org.springframework.cache.jcache.JCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import java.io.IOException;
+
+@Configuration
+public class EhcacheConfig {
+
+    @Bean(name = "ehCacheManager")
+    public org.springframework.cache.CacheManager cacheManager() throws IOException {
+        CachingProvider cachingProvider = Caching.getCachingProvider("org.ehcache.jsr107.EhcacheCachingProvider");
+        CacheManager manager = cachingProvider.getCacheManager(
+                new ClassPathResource("/ehcache.xml").getURI(),
+                getClass().getClassLoader());
+
+        return new JCacheCacheManager(manager);
+    }
+}
