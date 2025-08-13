c3b4d55e08594323eed62512497d31e67445ed35
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-13 16:12:29 +0900

    [Chore] EhCache 의존성 추가

--- src/main/java/org/sopt/Main.java
+++ src/main/java/org/sopt/Main.java
@@ -1,15 +1,15 @@
 package org.sopt;
 
 
-import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableCaching
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
